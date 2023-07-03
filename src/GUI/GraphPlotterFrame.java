9eec3607c7ccd3799f6a44a5c1fe122e3aa6a833
Fabian Noll <Fabian.Noll@googlemail.com>
2023-07-03 18:50:51 +0200

    Change Layout to JSplitPane for better user experience

--- src/GUI/GraphPlotterFrame.java
+++ src/GUI/GraphPlotterFrame.java
@@ -3,49 +3,30 @@ package GUI;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.awt.*;
 
 public class GraphPlotterFrame extends JFrame {
     public GraphPlotterFrame() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(1400,900); //Sets the Size in case the Window is not maximized
+        this.setSize(1400,900);
         this.setExtendedState(MAXIMIZED_BOTH);
         this.setResizable(true);
         this.setTitle("Function Plotter");
-        ImageIcon logo = new ImageIcon(".\\FunctionPlotterLogo.png"); //"converts" the .png into an ImageIcon
+        ImageIcon logo = new ImageIcon(".\\FunctionPlotterLogo.png");
         this.setIconImage(logo.getImage());
 
-        ControlPanel controlPanel = new ControlPanel();
         GraphPanel graphPanel = new GraphPanel();
-
-        GridBagLayout frameLayout = new GridBagLayout();
-        setLayout(frameLayout);
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.insets = new Insets(0,0,0,0);   //Distance to Grid-"Lines" (that are not visible)
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        frameLayout.setConstraints(controlPanel, gbc);
-        this.add(controlPanel);
+        ControlPanel controlPanel = new ControlPanel(graphPanel);
         controlPanel.addChangeListener(new TabChangeListener());
 
-
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.weightx = 10;
-        // TODO: Find optimal weighting between the two panels, current weighting is 1:10 (consult Team)
-        gbc.weighty = 1;
-        frameLayout.setConstraints(graphPanel, gbc);
-        this.add(graphPanel);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, controlPanel, graphPanel);
+        splitPane.setOneTouchExpandable(true); // Adds buttons to collapse/expand the panels
+        splitPane.setResizeWeight(0.05); // How the space is distributed between the two components. 0.5 means 50%-50%
+        this.add(splitPane);
 
         this.setVisible(true);
     }
 
-    private class TabChangeListener implements ChangeListener {
+    private static class TabChangeListener implements ChangeListener {
         @Override
         public void stateChanged(ChangeEvent e) {
             //TODO: Clear Graph Panel
