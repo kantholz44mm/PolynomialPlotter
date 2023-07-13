12bff6d7598f5988ea143e1ecaa99b8221ff0d5a
FabianNoll <Fabian.Noll@googlemail.com>
2023-07-13 09:19:39 +0200

    Fix Help Window

--- src/GUI/ParametricFunctionControl.java
+++ src/GUI/ParametricFunctionControl.java
@@ -26,7 +26,7 @@ public class ParametricFunctionControl extends JPanel {
         public class HelpActionListener implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: Display HelpWindow
+                HelpWindow.getInstance().setVisible(true);
             }
         }
 
